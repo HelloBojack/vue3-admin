8cec7665707cd5bb7a83a599743d821332b17a3f
bojack <695668678@qq.com>
2021-04-22 22:58:54 +0800

    vue3

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TestDialog></TestDialog>
+</template>
+
+<script setup>
+  import TestDialog from './components/TestDialog.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app/src/main.js
@@ -0,0 +1,77 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+const app = createApp(App)
+
+class Drager {
+  static zIndex = 1;
+  constructor(el, option = {}) {
+    this.el = el;
+    this.x = 0;
+    this.y = 0;
+    this.option = option;
+    this.init();
+  }
+  init() {
+    this.setEleStyle(this.option || {});
+    this.el.onmousedown = (e) => {
+      this.onMouseDown(e)
+      this.el.setCapture && this.el.setCapture() //全局捕获
+      return false
+    }
+  }
+  //样式设置
+  setEleStyle(option) {
+    for (const key in option) {
+      this.el.style[key] = option[key]
+    }
+  }
+  //按下ele
+  onMouseDown(e) {
+    let zIndex = getComputedStyle(this.el).getPropertyValue('z-index');
+    zIndex = isNaN(zIndex) ? 1 : zIndex
+    Drager.zIndex = Drager.zIndex > zIndex ? Number(Drager.zIndex) + 1 : Number(zIndex) + 1
+    this.setEleStyle({ "zIndex": Drager.zIndex, position: 'fixed', 'cursor': 'move' })
+    this.x = e.clientX - this.el.offsetLeft;
+    this.y = e.clientY - this.el.offsetTop;
+    document.onmousemove = (e) => this.onMouseMove(e);
+    document.onmouseup = (e) => this.onMouseUp(e)
+
+  }
+  //移动move
+  onMouseMove(e) {
+    let X = e.clientX - this.x
+    let Y = e.clientY - this.y;
+    if (X < 10 - this.el.offsetWidth) {
+      X = 10 - this.el.offsetWidth
+    } else if (X > document.documentElement.clientWidth - 10) {
+      X = document.documentElement.clientWidth - 10
+    }
+    if (Y < 10 - this.el.clientHeight) {
+      Y = 10 - this.el.clientHeight
+    } else if (Y > document.documentElement.clientHeight - 10) {
+      Y = document.documentElement.clientHeight - 10
+    }
+    this.el.style.left = X + 'px'
+    this.el.style.top = Y + 'px'
+  }
+  //释放
+  onMouseUp(e) {
+    document.onmousemove = null
+    document.onmouseup = null
+    this.setEleStyle({ 'cursor': 'pointer' })
+    this.el.setCapture && this.el.setCapture() //释放全局捕获
+  }
+}
+
+app.directive('drager', {
+  mounted(el, binding) {
+    new Drager(el, binding.value || {})
+  }
+});
+
+app.directive('', {
+
+})
+app.use(ElementPlus).mount('#app')
